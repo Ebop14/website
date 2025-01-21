09d21a84eef61a28262cf28110bddfd196a5d7f4
Ethan Child <echild@Ethans-MacBook-Pro.local>
2025-01-21 13:39:26 -0500

    adding font

--- hack-a-thing/src/App.js
+++ hack-a-thing/src/App.js
@@ -40,10 +40,12 @@ function App() {
 
     </div>
 
+    <div></div>
+
     <div className="BigParagraph">
       <img src='https://media.licdn.com/dms/image/v2/C4D03AQGj3D50zP4hUQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1609527769822?e=1743033600&v=beta&t=kSKPnVTEOTrdsIqd-ec_9XqCvh1_JQEg3hzY1cdPuAk'
       ></img>
-      <h1>
+      <h1 style={{fontFamily:'Dimitri'}}>
           Hi! My name is Ethan, and I'm a Junior at Dartmouth double majoring in Economics and CS.
         </h1>
     </div>
@@ -68,9 +70,7 @@ function App() {
         description="This is not working?">
         </HorizontalItems>
       </span>
-
-        
-
+      
     </div>
     </>
   );
